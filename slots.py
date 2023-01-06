2d630afc3167ab15ec5ceb31a3a619ea341ec1f5
eat-sleep-code <info@eat-sleep-code.com>
2023-01-06 11:45:52 -0700

    Begin work on scoring

--- slots.py
+++ slots.py
@@ -5,22 +5,36 @@ import pygame
 import random
 import RPi.GPIO as GPIO
 
-
+# File paths
 gameTheme = 'classic'
 appRoot = os.getcwd() + '/slots/'
 imageRoot = appRoot + 'images/' + gameTheme
 audioRoot = appRoot + 'audio/' + gameTheme
+
+# Positioning
 reelOffsetX = 45
 reelOffsetY = 10
 reelGutter = 45
 reelCount = 3
 reelScoringOffset = 400 # Where is the payline relative to the top of the screen?
 reelSequence = [500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200,2300,2400]
+payoutX = 45
+payoutY = 750
+freePlaysX = 295
+freePlaysY = 750
 
+# Scoring
+spinCost = 10
+currentPayout = 0
+currentFreePlays = 20
+wildPayout = 10
+wildFreePlays = 3
 
 
 class Game(object):
 	def __init__(self):
+		self.font = pygame.font.Font('freesansbold.ttf', 32)
+
 		self.mask = pygame.image.load(os.path.join(imageRoot, 'mask.png'))
 
 		self.reel01 = pygame.image.load(os.path.join(imageRoot, 'reel-01.jpg'))
@@ -61,9 +75,19 @@ class Game(object):
 
 
 	def spin(self, surface, screenX, screenY):
+		global currentFreePlays
+		global currentPayout
 		reelWidth = (screenX - (reelOffsetX * 2) - ((reelCount -1) * reelGutter)) / reelCount
 
 		# Spinning...
+		if currentFreePlays > 0:
+			currentFreePlays = currentFreePlays - 1;
+		else:
+			currentPayout = int(currentPayout) - (spinCost)
+
+		currentFreePlaysText = self.font.render(str(currentFreePlays), True, (255, 255, 255))
+		currentPayoutText = self.font.render(str(currentPayout), True, (255, 255, 255))
+
 		reelSpins = random.randrange(7, 15)
 		pygame.mixer.Sound.play(self.audioSpinning, 10)
 		for i in range(0, reelSpins):
@@ -86,6 +110,8 @@ class Game(object):
 					surface.blit(self.reel03Spinning, (reelOffsetX + (reelWidth * 2) + (reelGutter * 2), y * -1.2))
 				
 				surface.blit(self.mask, (0,0))
+				surface.blit(currentFreePlaysText, (freePlaysX, freePlaysY))
+				surface.blit(currentPayoutText, (payoutX, payoutY))
 				pygame.display.update()
 				
 		pygame.mixer.Sound.stop(self.audioSpinning)		
@@ -97,7 +123,15 @@ class Game(object):
 		
 		winnings = self.checkWinnings(reel01payline, reel02payline, reel03payline)
 		if (winnings[0] != "none"):
-			pygame.mixer.Sound.play(self.audioJackpot)
+			if (winnings[0] == "wildPartial"):
+				# Wilds
+				pygame.mixer.Sound.play(self.audioPoints)
+			else:
+				# Jackpot
+				pygame.mixer.Sound.play(self.audioJackpot)
+
+			currentPayout = int(currentPayout) + int(winnings[1])
+			currentFreePlays = int(currentFreePlays) + int(winnings[2])
 			for i in range(0, 20):
 				GPIO.output(27,GPIO.HIGH)
 				pygame.time.delay(100)
@@ -111,28 +145,37 @@ class Game(object):
 		
 		# Get the wild positions...
 		wild = jmespath.search("symbols[?name == 'wild'].positions | [0]", self.symbolMapping);
-		
+		wildCount = 0
 		# Check each reel for a non-wild symbol, if exists adjust match expressions 
 		matchExpression01 = "(positions[0] >= `0`)"
 		if (reel01 != wild[0]):
 			matchExpression01 = "(positions[0] == `" + str(reel01) + "`) "
+		else:
+			wildCount = wildCount + 1
 
 		matchExpression02 = "(positions[1] >= `0`)"
 		if (reel02 != wild[1]):
 			matchExpression02 = "(positions[1] == `" + str(reel02) + "`) "
+		else: 
+			wildCount = wildCount + 1
 
 		matchExpression03 = "(positions[2] >= `0`)"
 		if (reel03 != wild[2]):
 			matchExpression03 = "(positions[2] == `" + str(reel03) + "`) "
+		else: 
+			wildCount = wildCount + 1
 
 		# Search for matches...
 		matches = jmespath.search("symbols[?" + matchExpression01 + "&&" + matchExpression02 + "&&" + matchExpression03 + "]", self.symbolMapping)
-		if matches:
-			print("You win!", matches)
+		if matches or wildCount == 3:
+			print("Jackpot!", matches)
 			winningMatch = json.loads(json.dumps(matches[0]))
 			return winningMatch["name"], winningMatch["payout"], winningMatch["freePlays"]
+		elif wildCount > 0: 
+			print("You got " + str(wildCount) + " wild slots!")
+			return "wildPartial", wildPayout * wildCount, wildFreePlays * wildCount 
 		else:
-			print("You lose", matches)
+			print("You got nothin'", matches)
 			return "none", 0, 0	
 	
 
